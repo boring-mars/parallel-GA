d9ace6927e14cc919e3ba36f0207ba52549a8fe3
Mars <blackball1998@outlook.com>
2025-07-29 19:09:35 +0100

    optimise pick parent

--- RunningConfig.h
+++ RunningConfig.h
@@ -3,11 +3,13 @@
 
 class RunningConfig {
 public:
-    static const int gene_length = 12;
+    static const int gene_length = 8;
     static const int max_generation = 1000000;
-    static const int population_size = 200;
+    static const int population_size = 100;
+    static const int cellular_row_size = 10;
+    static const int cellular_col_size = population_size / cellular_row_size;
     static constexpr double mutation_rate = 0.1;
-    static const int total_solution_num = 50;
+    static const int total_solution_num = 92;
 };
 
 #endif //PARALLEL_GA_RUNNINGCONFIG_H
